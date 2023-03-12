3b476f0b598c24ed5a0563d553459b703c24c42c
aroldo <asouzaleite@gmx.de>
2023-03-12 13:39:06 +0100

    copied from ¯/Musik.

---
+++ music_cd_utils.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# @file: wav_cd_utils.py
+# @date: 2023-03-12T12:45:24
+# @author: Aroldo Souza-Leite
+# @description: 
+"""
+Some utilities for processing files copied from
+wav cds.
+"""
+import os, sys, logging
+__level__ = logging.DEBUG
+
+
+
+######################### <_get_slog>   #########
+def _get_slog ( level = __level__):
+    "Wrapper for the get_slog function"
+
+    import  random, string
+
+    class SLogFormatter(logging.Formatter):
+        "A log formatter for SLogHandler"
+        fmt = "%(levelname)s:%(name)s:%(module)s.%(funcName)s:%(lineno)s\n%(message)s"
+        def __init__(self, fmt = None):
+            if fmt is None:
+                fmt = SLogFormatter.fmt
+            super().__init__(fmt = fmt)
+
+    class SLogHandler(logging.StreamHandler):
+        "Simple logging handler."
+        def __init__(self, stream = sys.stderr, level = __level__):
+            super().__init__(stream = stream)
+            self.setLevel(level)
+            self.setFormatter(SLogFormatter())
+
+    def get_slog(handler = None, level = __level__):
+        "Make a simple logger"
+        if handler is None:
+            handler = SLogHandler()
+        handler.setLevel(level)
+        random_string = "".join(random.sample(string.ascii_letters, 4))
+        log_name  = __name__ + "-" + random_string
+        slog = logging.getLogger(log_name)
+        slog.addHandler(handler)
+        slog.setLevel(level)
+        return slog
+    return get_slog(level = level)
+######################### </_get_slog>  #########
+
+_slog = _get_slog(level = __level__)
+
+## to unhide a code block got to the block top and press "<f5> h"
+
+
+def new_track_name(track_name):
+    """
+    From Track 1.wav to 01.wav.
+    From Track 12.wav to 12.wav.
+    """
+    track_number_plus_ext = track_name.split(" ")[1]
+    new_name = "{:0>6}".format(track_number_plus_ext)
+    return new_name
+
+def test_new_track_name():
+    """
+    Testet es on Track 1.wav
+    """
+    _slog.debug("beginning of function")
+    old_name = "Track 1.wav"
+    new_name = new_track_name(old_name)
+    assert new_name == "01.wav"
+    _slog.debug("end of function")
+
+
+def _script():
+    """
+    Runs if this module is called as a
+    Python script.
+    """
+    print(sys.argv[0])
+    test_new_track_name()
+if __name__ == "__main__":
+   _script()
+
+
+
+
